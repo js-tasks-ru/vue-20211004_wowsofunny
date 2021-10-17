84e075269830948a88115739663a01707159705e
Pavel <pavel_9_5@mail.ru>
2021-10-17 16:24:43 +0300

    Solve VueCalculator task

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,33 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const Root = defineComponent({
+  name: 'App',
+  data() {
+    return {
+      numFirst: 0,
+      numSecond: 0,
+      operand: 'sum',
+    };
+  },
+  computed: {
+    calcValue() {
+      if (this.operand === 'sum') {
+        return this.numFirst + this.numSecond;
+      }
+      if (this.operand === 'subtract') {
+        return this.numFirst - this.numSecond;
+      }
+      if (this.operand === 'multiply') {
+        return this.numFirst * this.numSecond;
+      }
+      if (this.operand === 'divide') {
+        return this.numFirst / this.numSecond;
+      }
+      return null;
+    },
+  },
+});
+
+const app = createApp(Root);
+
+app.mount('#app');
