d2628f371975265a825069b99354776a2902104c
Pavel <pavel_9_5@mail.ru>
2021-12-12 18:26:21 +0300

    Solve 04-MeetupTitle

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,8 +1,8 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
-function fetchMeetupById(meetupId) {
+/* function fetchMeetupById(meetupId) {
   return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
     if (response.ok) {
       return response.json();
@@ -12,6 +12,42 @@ function fetchMeetupById(meetupId) {
       });
     }
   });
-}
+} */
 
 // Требуется создать Vue приложение
+
+const Root = defineComponent({
+  data() {
+    return {
+      meetupId: 1,
+      meetupTitle: '',
+    };
+  },
+  watch: {
+    async meetupId() {
+      await this.fetchMeetupById(this.meetupId)
+        .then((data) => this.meetupTitle = data.title)
+    }
+  },
+  async mounted() {
+    await this.fetchMeetupById(this.meetupId)
+        .then((data) => this.meetupTitle = data.title)
+  },
+  methods: {
+    fetchMeetupById(meetupId) {
+      return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          return response.json().then((error) => {
+            throw error;
+          });
+        }
+      });
+    },
+  },
+});
+
+const app = createApp(Root);
+
+app.mount('#app');
