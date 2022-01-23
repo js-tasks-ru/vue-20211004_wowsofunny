339788b0abe8c39e0f7dd0cdb6fa00d66cf0ff73
Pavel <pavel_9_5@mail.ru>
2022-01-23 16:58:59 +0300

    Solve PageMeetup task

--- 02-components/07-PageMeetup/PageMeetup.js
+++ 02-components/07-PageMeetup/PageMeetup.js
@@ -1,7 +1,8 @@
 import { defineComponent } from './vendor/vue.esm-browser.js';
 import UiContainer from './UiContainer.js';
 import UiAlert from './UiAlert.js';
-// import { fetchMeetupById } from './meetupService.js';
+import MeetupView from './MeetupView.js';
+import { fetchMeetupById } from './meetupService.js';
 
 export default defineComponent({
   name: 'PageMeetup',
@@ -9,18 +10,61 @@ export default defineComponent({
   components: {
     UiAlert,
     UiContainer,
+    MeetupView,
+  },
+  props: {
+    meetupId: {
+      type: Number,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      currentMeetup: {},
+      loading: false,
+      isError: false,
+      errorMsg: '',
+    };
+  },
+  watch: {
+    async meetupId() {
+      await this.getCurrentMeetup(this.meetupId);
+    },
+  },
+  async beforeMount() {
+    await this.getCurrentMeetup(1);
+  },
+  async mounted() {
+    await this.getCurrentMeetup(this.meetupId);
+  },
+
+  methods: {
+    async getCurrentMeetup(meetupId) {
+      this.loading = true;
+      this.isError = false
+      try {
+        await fetchMeetupById(meetupId).then((result) => {
+          this.currentMeetup = result;
+          this.loading = false;
+        });
+      } catch(error) {
+        this.loading = false;
+        this.isError = true;
+        this.errorMsg = error.message;
+      }
+    },
   },
 
   template: `
     <div class="page-meetup">
-      <!-- meetup view -->
+      <meetup-view v-if="!loading && !isError" :meetup="currentMeetup"></meetup-view>
 
-      <ui-container>
+      <ui-container v-if="loading">
         <ui-alert>Загрузка...</ui-alert>
       </ui-container>
 
       <ui-container>
-        <ui-alert>error</ui-alert>
+        <ui-alert v-if="isError">{{ errorMsg }}</ui-alert>
       </ui-container>
     </div>`,
 });
